349a730627475a9885fa336c7eeb73a0986f1f6f
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2025-09-14 22:39:35 +0100

    Add whatsapp messaging

---
+++ src/SharedKernel/Configs/WhatsAppConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedKernel.Configs;
+
+public class WhatsAppConfig
+{
+    public required string AppId { get; init; }
+    public required string ApiKey { get; init; }
+    public required string PhoneNumberId { get; init; }
+}
