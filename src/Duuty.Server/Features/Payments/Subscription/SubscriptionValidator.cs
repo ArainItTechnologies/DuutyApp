445501ea90a5dd44d5dd0245264499292b1c9e00
PDommalapati <prasanna.dommalapati@gardinerbros.co.uk>
2025-09-21 21:05:08 +0100

    Add Validators

---
+++ src/Duuty.Server/Features/Payments/Subscription/SubscriptionValidator.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Duuty.Server.Features.Payments.Subscription;
+
+public class SubscriptionValidator : Validator<SubscriptionRequest>
+{
+    public SubscriptionValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty()
+            .WithMessage("UserId is required.");
+        RuleFor(x => x.Plan)
+            .NotEmpty()
+            .WithMessage("Plan is required.");
+    }
+}
---
+++ src/Duuty.Server/Features/Payments/VerifyPayment/VerifyPaymentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Duuty.Server.Features.Payments.VerifyPayment;
+
+public class VerifyPaymentValidator : Validator<VerifyPaymentRequest>
+{
+    public VerifyPaymentValidator()
+    {
+        RuleFor(x => x.PaymentId)
+            .NotEmpty()
+            .WithMessage("PaymentId is required.");
+
+        RuleFor(x => x.OrderId)
+            .NotEmpty()
+            .WithMessage("OrderId is required.");
+
+        RuleFor(x => x.Signature)
+            .NotEmpty()
+            .WithMessage("Signature is required.");
+    }
+}
